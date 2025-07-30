1c5adc7da083fac76067ae450eca441c66783815
Muhammad Nauman <nauman33183@gmail.com>
2025-07-30 06:05:30 -0700

    vercel host

--- client/src/utils/exports.tsx
+++ client/src/utils/exports.tsx
@@ -1,2 +1,2 @@
-export const backendUrl = "http://localhost:5000/api/v1/";
+export const backendUrl = "https://finance-fire-svpq.vercel.app/api/v1/";
 export const googleClientId = "268486767053-den8fcjmuhauvah75hp80a01rr0i6580.apps.googleusercontent.com";
