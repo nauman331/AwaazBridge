58a7c4b2d29fd6c4424a3ef567e5b5ba5a37a120
Muhammad Nauman <nauman33183@gmail.com>
2025-09-11 00:26:09 -0700

    backend url

--- client/src/utils/exports.tsx
+++ client/src/utils/exports.tsx
@@ -12,7 +12,8 @@ import {
     MessageSquare
 } from 'lucide-react'
 
-export const backendUrl = "https://finance-fire-svpq.vercel.app";
+export const backendUrl = "https://finance-fire-svpq.vercel.app/api/v1/";
+
 
 export const googleClientId = "268486767053-den8fcjmuhauvah75hp80a01rr0i6580.apps.googleusercontent.com";
 
