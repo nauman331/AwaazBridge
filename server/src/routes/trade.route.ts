2406769941be0a68b8e5eeb83cffb6720148728c
Muhammad Nauman <nauman33183@gmail.com>
2025-10-01 01:57:06 -0700

    unnecessary code

--- server/src/routes/trade.route.ts
+++ server/src/routes/trade.route.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { getTradesByUserId, placeTradeREST } from "../controllers/trade.controller";
+import { getTradesByUserId } from "../controllers/trade.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.get("/getcurrenttrades/:userId", authMiddleware, getTradesByUserId);
-router.post("/placetrade", placeTradeREST);
 
 
 export default router;
